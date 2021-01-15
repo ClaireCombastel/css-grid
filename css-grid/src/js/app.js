0b6edde8902f4c03349469afd9798e2e62c638a7
ClaireCombastel <combastel.cl@gmail.com>
2021-01-15 23:24:03 +0100

    #3 block choice

--- css-grid/src/js/app.js
+++ css-grid/src/js/app.js
@@ -1,13 +1,11 @@
-const itemsSlide = document.querySelectorAll('.container-slides img');
+const itemsSlide = document.querySelectorAll(".container-slides img");
 const nbSlides = itemsSlide.length;
-const next = document.querySelector('.right');
-const previous = document.querySelector('.left');
+const next = document.querySelector(".right");
+const previous = document.querySelector(".left");
 let count = 0;
 
-next.addEventListener ('click', nextSlide);
-
 function nextSlide(){
-    itemsSlide[count].classList.remove('active');
+    itemsSlide[count].classList.remove("active");
 
     if(count < nbSlides -1) {
         count++;
@@ -15,13 +13,13 @@ function nextSlide(){
         count = 0;
     }
 
-    itemsSlide[count].classList.add('active');
+    itemsSlide[count].classList.add("active");
 }
 
-previous.addEventListener('click', previousSlide);
+next.addEventListener ("click", nextSlide);
 
 function previousSlide(){
-    itemsSlide[count].classList.remove('active');
+    itemsSlide[count].classList.remove("active");
 
     if(count > 0) {
         count --;
@@ -29,5 +27,7 @@ function previousSlide(){
         count = nbSlides - 1;
     }
 
-    itemsSlide[count].classList.add('active');
+    itemsSlide[count].classList.add("active");
 }
+
+previous.addEventListener("click", previousSlide);
